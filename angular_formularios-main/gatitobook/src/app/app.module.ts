fb01d3e3195643583086f2189d5283946a4362b7
ElizioMartins <elizio.martins@hotmail.com>
2021-08-25 13:52:56 -0300

    Atualizações gerais

--- angular_formularios-main/gatitobook/src/app/app.module.ts
+++ angular_formularios-main/gatitobook/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
+import { RodapeModule } from './componentes/rodape/rodape.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CabecalhoModule,
+    RodapeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
