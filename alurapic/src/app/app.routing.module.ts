7d25f50f53bf2d03a02bdcfffda06e5d7c9d89c1
ElizioMartins <elizio.martins@hotmail.com>
2021-08-27 10:55:09 -0300

    Atualização final parte3

--- alurapic/src/app/app.routing.module.ts
+++ alurapic/src/app/app.routing.module.ts
@@ -5,10 +5,22 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { AuthGuard } from './core/auth/auth.guard';
+import { PhotoDetailsComponent } from './photos/photo-details/photo-details.component';
 
 const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+    { 
+        path: 'home',
+        loadChildren: './home/home.module#HomeModule'
+    },              
     { 
         path: 'user/:userName', 
+        pathMatch: 'full',
         component: PhotoListComponent,
         resolve: {
             photos: PhotoListResolver
@@ -16,17 +28,26 @@ const routes: Routes = [
     },
     { 
         path: 'p/add', 
-        component: PhotoFormComponent 
+        component: PhotoFormComponent,
+        canActivate: [AuthGuard]
     },
     { 
-        path: '**', 
+        path: 'p/:photoId', 
+        component: PhotoDetailsComponent,
+    }, 
+    { 
+        path: 'not-found', 
         component: NotFoundComponent 
+    },     
+    { 
+        path: '**', 
+        redirectTo: 'not-found'
     }  
 ];
 
 @NgModule({
     imports: [ 
-        RouterModule.forRoot(routes) 
+        RouterModule.forRoot(routes, { useHash: true } ) 
     ],
     exports: [ RouterModule ]
 })
